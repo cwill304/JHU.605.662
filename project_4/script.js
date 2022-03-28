299fa3e4892ab0dc4ddb3c43ed382b00d8425c39
cwill304 <cwill304@jh.edu>
2022-03-27 22:35:12 -0500

    Updated script.js

--- project_4/script.js
+++ project_4/script.js
@@ -1,3 +1,32 @@
 (function () {
-    console.log("hello, world");
+
+    // Store all node data
+    var nodes = [];
+
+    // GET the CSV file
+    var request = new XMLHttpRequest();
+    request.open('GET', 'data.csv', true);
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        convertCSV(this.response);
+      }
+    };
+    request.send();
+
+    // Convert CSV format to JSON
+    function convertCSV(csv) {
+        var rows = csv.split(/\r?\n/);
+        rows.forEach((row, rIndex )=> {
+            var obj = {id: rIndex};
+            var values = row.split(",");
+            values.forEach((value, vIndex) => {
+                obj[vIndex] = value;
+            });
+            nodes.push(obj);
+        });
+        console.log(nodes);
+    }
+
+
+
 })();
